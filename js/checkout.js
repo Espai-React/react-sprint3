0b17d6de28a5590dbd5d147f05f57e6de31fe631
llopfilms <llopfilms@gmail.com>
2024-05-29 01:05:43 +0200

    Actualització

--- js/checkout.js
+++ js/checkout.js
@@ -1,56 +1,66 @@
-
 // Exercise 7
 function validate() {
+  let error = 0;
 
-	let error = 0;
-
-	// RegExp
-	let regExpName = /^[-'a-zA-ZÀ-ÿ\s]{3,}$/i;
-	let regExpEmail = /^[-'a-zA-Z0-9_+]+@[-'a-zA-Z0-9_+]+\.[a-z]{2,3}$/i;
-	let regExpPassword = /^.{4,8}$/;
-	let regExpAdress = /^.{3,}$/;
-	let regExhPhone = /^\d{9}$/;
+  // RegExp
+  let regExpName = /^[-'a-zA-ZÀ-ÿ\s]{3,}$/i;
+  let regExpEmail = /^[-'a-zA-Z0-9_+]+@[-'a-zA-Z0-9_+]+\.[a-z]{2,3}$/i;
+  let regExpPassword = /^.{4,8}$/;
+  let regExpAdress = /^.{3,}$/;
+  let regExhPhone = /^\d{9}$/;
 
   // Get the input fields
-	let fName = document.getElementById("fName");
-	let fLastN = document.getElementById("fLastN");
-	let fEmail = document.getElementById("fEmail");
-	let fpassword = document.getElementById("fPassword");
-	let fAddress = document.getElementById("fAddress");
-	let fPhone = document.getElementById("fPhone");
+  let fName = document.getElementById('fName');
+  let fLastN = document.getElementById('fLastN');
+  let fEmail = document.getElementById('fEmail');
+  let fpassword = document.getElementById('fPassword');
+  let fAddress = document.getElementById('fAddress');
+  let fPhone = document.getElementById('fPhone');
 
-	// Validate fields entered by the user: name, phone, password, and email
+  // Validate fields entered by the user: name, phone, password, and email
 
-	//Versió amb bucle for i classList, i amb classes "is-valid" i "is-invalid" de Bootstrap
-	let validation = [regExpName, regExpName, regExpEmail, regExpPassword, regExpAdress, regExhPhone, fName, fLastN, fEmail, fpassword, fAddress, fPhone];
-	
-	for (i = 0; i < 6; i++) {
+  //Versió amb bucle for i classList, i amb classes "is-valid" i "is-invalid" de Bootstrap
+  let validation = [
+    regExpName,
+    regExpName,
+    regExpEmail,
+    regExpPassword,
+    regExpAdress,
+    regExhPhone,
+    fName,
+    fLastN,
+    fEmail,
+    fpassword,
+    fAddress,
+    fPhone,
+  ];
 
-		if (validation[i+6].value == "" || validation[i].test(validation[i+6].value) == false) {
+  for (i = 0; i < 6; i++) {
+    if (
+      validation[i + 6].value == '' ||
+      validation[i].test(validation[i + 6].value) == false
+    ) {
+      validation[i + 6].classList.add('is-invalid');
+      validation[i + 6].classList.remove('is-valid');
+      error++;
+    } else {
+      validation[i + 6].classList.add('is-valid');
+      validation[i + 6].classList.remove('is-invalid');
+    }
+  }
 
-			validation[i+6].classList.add("is-invalid");
-			validation[i+6].classList.remove("is-valid");
-			error++;
+  //Versió amb elements del DOM
 
-		}else{
-
-			validation[i+6].classList.add("is-valid");
-			validation[i+6].classList.remove("is-invalid");
-		}
-	}
-	
-	//Versió amb elements del DOM
-
-	// Get the error elements
-	/* let errorName = document.getElementById("errorName");
+  // Get the error elements
+  /* let errorName = document.getElementById("errorName");
 	let errorLastN = document.getElementById("errorLastN");
 	let errorEmail = document.getElementById("errorEmail");
 	let errorPassword = document.getElementById("errorPassword");
 	let errorAddress = document.getElementById("errorAddress");
 	let errorPhone = document.getElementById("errorPhone"); */
-	
-	// Agafem els elements de no-error
-	/* let correctName = document.getElementById("correctName");
+
+  // Agafem els elements de no-error
+  /* let correctName = document.getElementById("correctName");
 	let correctLastN = document.getElementById("correctLastN");
 	let correctEmail = document.getElementById("correctEmail");
 	let correctPassword = document.getElementById("correctPassword");
@@ -136,11 +146,10 @@ function validate() {
 	}
 	*/
 
-	/* if(error > 0){
+  /* if(error > 0){
 		alert("Error");
 
 	}else{
 		alert("OK");
 	} */
-
 }
--- js/shop.js
+++ js/shop.js
@@ -1,5 +1,3 @@
-
-
 // Array with products (objects) added directly with push(). Products in this array are repeated.
 const cartList = [];
 
@@ -12,62 +10,57 @@ let counter = 0;
 
 // Exercise 1
 function buy(id) {
-
   // 1. Loop for to the array products to get the item to add to cart
-  
-  let i, productChosen, product, found = false;
+
+  let i,
+    productChosen,
+    product,
+    found = false;
   for (i = 0; i < products.length; i++) {
+    // Tornem els objectes al format original, si ja han estat triats abans
+    product = products[i];
 
-      // Tornem els objectes al format original, si ja han estat triats abans
-      product = products[i];
-
-      if (product.id == id) {
-
-        productChosen = {...product};
-        found = true;
-      }
+    if (product.id == id) {
+      productChosen = { ...product };
+      found = true;
+    }
   }
 
   // 2. Add found product to the cartList array
   // Incropora comptador d'articles per la icona del botó "Cart"
 
   if (found == true) {
-
     cartList.push(productChosen);
     counter++;
   }
-  document.getElementById("count_product").innerHTML = counter;
+  document.getElementById('count_product').innerHTML = counter;
 }
 
 // Exercise 2
 function cleanCart() {
-
   cartList.length = 0;
   // Reinici comptador
   counter = 0;
-  document.getElementById("count_product").innerHTML = counter;
+  document.getElementById('count_product').innerHTML = counter;
   generateCart();
   printCart();
-
 }
 
 // Exercise 3
 function calculateTotal() {
-
   // Calculate total price of the cart using the "cartList" array
 
-  let i, price = 0, totalPrice = 0;
+  let i,
+    price = 0,
+    totalPrice = 0;
   for (i = 0; i < cartList.length; i++) {
-
-      price = cartList[i].price;
-      totalPrice += price;
+    price = cartList[i].price;
+    totalPrice += price;
   }
-
 }
 
 // Exercise 4
 function generateCart() {
-
   // Using the "cartlist" array that contains all the items in the shopping cart,
   // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
 
@@ -75,77 +68,84 @@ function generateCart() {
 
   let i, cartListItem, cartItem;
   for (i = 0; i < cartList.length; i++) {
+    cartListItem = { ...cartList[i] };
+    const cartItemIndex = cart.findIndex(
+      (element) => element.id === cartListItem.id
+    );
 
-      cartListItem = {...cartList[i]};
-      const cartItemIndex = cart.findIndex(element => element.id === cartListItem.id);
-      
-      if (cartItemIndex == -1) {
-
-          cart.push(cartListItem);
-          cartItem = cart.at(-1);
-          cartItem.quantity = 1;
-          
-      } else {
-				cartItem = cart[cartItemIndex];
-        cartItem.quantity++;
-          
-      }
-      cartItem.subTotal = cartItem.price * cartItem.quantity;
-      // preparem propietat
-      cartItem.subTotalWithDiscount = "not available";
-      
+    if (cartItemIndex == -1) {
+      cart.push(cartListItem);
+      cartItem = cart.at(-1);
+      cartItem.quantity = 1;
+    } else {
+      cartItem = cart[cartItemIndex];
+      cartItem.quantity++;
+    }
+    cartItem.subTotal = cartItem.price * cartItem.quantity;
+    // preparem propietat
+    cartItem.subTotalWithDiscount = 'not available';
   }
   applyPromotionsCart();
 }
 
 // Exercise 5
 function applyPromotionsCart() {
-
   // Apply promotions to each item in the array "cart"
   // Funció applyPromotionsCart() afegit a la funció GenerateCart()
   // Nous càlculs de les promocions segons "percent" de "products"
 
   let i;
-  for(i = 0; i < cart.length; i++) {
-
+  for (i = 0; i < cart.length; i++) {
     const cartItem = cart[i];
 
-    if ((cartItem.id == 1 || cartItem.id == 3) && cartItem.quantity >= cartItem.offer.number ) {
-
-        cartItem.subTotalWithDiscount = Number((cartItem.subTotal - cartItem.subTotal * cartItem.offer.percent/100).toFixed(2));
+    if (
+      (cartItem.id == 1 || cartItem.id == 3) &&
+      cartItem.quantity >= cartItem.offer.number
+    ) {
+      cartItem.subTotalWithDiscount = Number(
+        (
+          cartItem.subTotal -
+          (cartItem.subTotal * cartItem.offer.percent) / 100
+        ).toFixed(2)
+      );
     }
   }
-
 }
 
 // Exercise 6
 function printCart() {
-
   // Fill the shopping cart modal manipulating the shopping cart dom
 
-  let i, list = "", cartItem, cartTotal = 0;
+  let i,
+    list = '',
+    cartItem,
+    cartTotal = 0;
   for (i = 0; i < cart.length; i++) {
-
     cartItem = cart[i];
 
-    list += "<tr>";
-    list += "<th scope='row'>" +cartItem.name+ "</th>";
-    list += "<td class='text-center fs-5'>" +cartItem.price+ "</td>";
-    list += "<td class='text-center fs-5'>" +cartItem.quantity+ "</td>";
-    if (cartItem.subTotalWithDiscount == "not available") {
-      list += "<td class= 'fs-5'>" +cartItem.subTotal+ "</td>";
+    list += '<tr>';
+    list += "<th scope='row'>" + cartItem.name + '</th>';
+    list += "<td class='text-center fs-5'>" + cartItem.price + '</td>';
+    list += "<td class='text-center fs-5'>" + cartItem.quantity + '</td>';
+    if (cartItem.subTotalWithDiscount == 'not available') {
+      list += "<td class= 'fs-5'>" + cartItem.subTotal + '</td>';
       cartTotal += cartItem.subTotal;
-    }else{
-      list += "<td class='fs-5'>" +cartItem.subTotalWithDiscount+ "</td>";
+    } else {
+      list += "<td class='fs-5'>" + cartItem.subTotalWithDiscount + '</td>';
       cartTotal += cartItem.subTotalWithDiscount;
     }
-    list += "<td class='text-center'><button type='button' class='btn btn-outline-dark btn-sm rounded-pill px-2 py-0' onclick='oneMoreToCart("+cartItem.id+")'>+</button></td>";
-    list += "<td class='text-center'><button type='button' class='btn btn-outline-dark btn-sm rounded-pill px-2 py-0' onclick='removeFromCart("+cartItem.id+")'>-</button></td>";
-    list +="</tr>";
-
+    list +=
+      "<td class='text-center'><button type='button' class='btn btn-outline-dark btn-sm rounded-pill px-2 py-0' onclick='oneMoreToCart(" +
+      cartItem.id +
+      ")'>+</button></td>";
+    list +=
+      "<td class='text-center'><button type='button' class='btn btn-outline-dark btn-sm rounded-pill px-2 py-0' onclick='removeFromCart(" +
+      cartItem.id +
+      ")'>-</button></td>";
+    list += '</tr>';
   }
-  document.getElementById("cart_list").innerHTML = list;
-  document.getElementById("total_price").innerHTML = cartTotal.toFixed(2);
+  document.getElementById('cart_list').innerHTML = list;
+  document.getElementById('total_price').innerHTML = cartTotal.toFixed(2);
 
   // Habilitat comptador de productes al botó "Cart"
 }
@@ -154,99 +154,87 @@ function printCart() {
 
 // Exercise 8
 function addToCart(id) {
-
   // Refactor previous code in order to simplify it
   // 1. Loop for to the array products to get the item to add to cart
   // 2. Add found product to the cart array or update its quantity in case it has been added previously.
 
   let productChosen, cartItem;
   // Els objectes no queden com originalment a products :-(
-  const productChosenIndex = products.findIndex(element => element.id === id);
+  const productChosenIndex = products.findIndex((element) => element.id === id);
 
-    if (productChosenIndex != -1) {
+  if (productChosenIndex != -1) {
+    productChosen = { ...products[productChosenIndex] };
+    counter++;
+  }
 
-      productChosen = {...products[productChosenIndex]};
-      counter++;
-    }
+  const cartItemIndex = cart.findIndex(
+    (element) => element.id === productChosen.id
+  );
 
-  const cartItemIndex = cart.findIndex(element => element.id === productChosen.id);
+  if (cartItemIndex == -1) {
+    cart.push(productChosen);
+    cartItem = cart.at(-1);
+    cartItem.quantity = 1;
+  } else {
+    cartItem = cart[cartItemIndex];
+    cartItem.quantity++;
+  }
 
-    if(cartItemIndex == -1) {
+  cartItem.subTotal = cartItem.price * cartItem.quantity;
+  // preparem propietat
+  cartItem.subTotalWithDiscount = 'not available';
 
-      cart.push(productChosen);
-      cartItem = cart.at(-1);
-      cartItem.quantity = 1;
-          
-    } else {
-
-      cartItem = cart[cartItemIndex];
-      cartItem.quantity++;
-    }
-
-    cartItem.subTotal = cartItem.price * cartItem.quantity;
-    // preparem propietat
-    cartItem.subTotalWithDiscount = "not available";
-  
   applyPromotionsCart();
-  document.getElementById("count_product").innerHTML = counter;
+  document.getElementById('count_product').innerHTML = counter;
 }
 
 // Exercise 9
 // Afegit botó '-' al modal per cada producte de Cart
 function removeFromCart(id) {
-
   let cartItem;
-  const cartItemIndex = cart.findIndex(element => element.id === id);
+  const cartItemIndex = cart.findIndex((element) => element.id === id);
 
-    if (cartItemIndex != -1) {
+  if (cartItemIndex != -1) {
+    cartItem = cart[cartItemIndex];
 
-      cartItem = cart[cartItemIndex];
-
-      if(cartItem.quantity > 1) {
-
-        cartItem.quantity--;
-        
-      }else{
-        cart.splice(cartItemIndex, 1);
-      }
+    if (cartItem.quantity > 1) {
+      cartItem.quantity--;
+    } else {
+      cart.splice(cartItemIndex, 1);
     }
+  }
 
-    cartItem.subTotal = cartItem.price * cartItem.quantity;
-    cartItem.subTotalWithDiscount = "not available";
-    applyPromotionsCart();
+  cartItem.subTotal = cartItem.price * cartItem.quantity;
+  cartItem.subTotalWithDiscount = 'not available';
+  applyPromotionsCart();
 
   printCart();
   counter--;
-  document.getElementById("count_product").innerHTML = counter;
-
+  document.getElementById('count_product').innerHTML = counter;
 }
 
 // Funcionalitat extra
 // Afegit botó '+' al modal per cada producte de Cart
 function oneMoreToCart(id) {
-
   let cartItem;
-  const cartItemIndex = cart.findIndex(element => element.id === id);
+  const cartItemIndex = cart.findIndex((element) => element.id === id);
 
-    if (cartItemIndex != -1) {
+  if (cartItemIndex != -1) {
+    cartItem = cart[cartItemIndex];
+    cartItem.quantity++;
+  }
 
-      cartItem = cart[cartItemIndex];
-      cartItem.quantity++;
-        
-    }
-  
-    cartItem.subTotal = cartItem.price * cartItem.quantity;
-    cartItem.subTotalWithDiscount = "not available";
-    applyPromotionsCart();
+  cartItem.subTotal = cartItem.price * cartItem.quantity;
+  cartItem.subTotalWithDiscount = 'not available';
+  applyPromotionsCart();
 
   printCart();
   counter++;
-  document.getElementById("count_product").innerHTML = counter;
-
+  document.getElementById('count_product').innerHTML = counter;
 }
 
-function open_modal(){
-  console.log("Open Modal");
+function open_modal() {
+  console.log('Open Modal');
   //generateCart();
   printCart();
 }
